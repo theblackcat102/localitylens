7e1a7da53441de2c33ce3013c63e7fda36ba2419
theblackcat102 <theblackcat102@github.com>
2024-03-20 09:06:13 +0800

    [feature] make project usable via pip

--- hybrid_search/utils.py
+++
@@ -1,7 +0,0 @@
-
-
-def chunks(lst, N):
-    for idx in range(0, len(lst), N):
-        yield lst[idx:idx+N]
-
-
--- node_parser/__init__.py
+++
@@ -1,3 +0,0 @@
-from typing import Callable, Optional
-
-global_tokenizer: Optional[Callable[[str], list]] = None
